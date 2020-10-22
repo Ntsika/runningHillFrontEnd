50eef617983805d762cb955ffdb367c621024ceb
Ntsika Mabongo <ntsikam@dlsys.co.za>
2020-10-22 02:39:09 +0200

    created method structure for sentence constructor component

--- src/app/sentence-constructor/sentence-constructor.component.ts
+++ src/app/sentence-constructor/sentence-constructor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {SentenceModule} from '../Models/sentence/sentence.module';
 import {WordModule} from '../Models/word/word.module';
+import {observable} from 'rxjs';
 
 @Component({
   selector: 'app-sentence-constructor',
@@ -10,11 +11,43 @@ import {WordModule} from '../Models/word/word.module';
 })
 export class SentenceConstructorComponent implements OnInit {
 
+  sentenceHistory: SentenceModule [] = [];
+  availableWords: WordModule[] = [];
+  wordTypes: string[] = [];
+  sentenceText: string = '';
+  selectedType: string = '';
+
   constructor() {
   //  populate typelist and sentence history
   }
 
   ngOnInit(): void {
+
   }
+
+  addWordToSentence(){
+
+  }
+
+  addSentence(){
+
+  }
+
+  getAllTypes(){
+
+  }
+
+  selectType(type: string){
+
+  }
+
+  populateWords(type: string){
+
+  }
+
+  updateSentenceHistory(){
+
+  }
+
 
 }
